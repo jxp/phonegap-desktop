d808d635e67f3b3e59f706628589fd4ce5ef22ac
Jonathan Prince <princej.ws01@hotmail.co.uk>
2012-03-05 22:12:36 +0000

    Fixed namespace issues. Added example plugin

---
+++ plugins/barcodescanner.js
@@ -0,0 +1,18 @@
+phonegapdesktop.internal.parseConfigFile('plugins/barcodescanner.json');
+
+if (!window.plugins){
+	window.plugins = {};
+}
+
+window.plugins.barcodeScanner = {
+	scan : function(successCallback, errorCallback) {
+		if (phonegapdesktop.internal.randomException()) {
+            errorCallback('A random error was generated');
+        } else {
+			successCallback(phonegapdesktop.internal.getDebugValue('barcodescanner', 'scans'));
+		}
+		
+	},
+	
+	encode : function(type, data, successCallback, errorCallback, options) {}
+}
